27626f0a21925749b9851af1963146c11bf74303
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-27 20:03:31 +0530

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:amazon_flutter/screens/sign_in_screen.dart';
 import 'package:amazon_flutter/utils/color_themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyC0BgOlCkfM_ZlLWc_dVaQRLemE2pzOJhY",
+            authDomain: "flutter-61bf6.firebaseapp.com",
+            projectId: "flutter-61bf6",
+            storageBucket: "flutter-61bf6.appspot.com",
+            messagingSenderId: "448647983859",
+            appId: "1:448647983859:web:a76edb637a5b1cda8d7d86",
+            measurementId: "G-X5KDCTMFVF"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const AmazonClone());
 }
 
