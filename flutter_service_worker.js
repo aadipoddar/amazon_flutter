f35ec4edaf08e7568987db92767a9f7c2e284257
aadipoddar <aadipoddar@users.noreply.github.com>
2022-05-04 17:52:39 +0000

    deploy: add76aa1393687215fc2ac330c881c0d1b425040

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "2775ad7c453f88f8623d04dc83c02fea",
-"/": "2775ad7c453f88f8623d04dc83c02fea",
+  "index.html": "86d795a2d055d95bf91cbfece7ca57ec",
+"/": "86d795a2d055d95bf91cbfece7ca57ec",
 "manifest.json": "8cd3aec3d7546dc09726b975bf09d954",
 "favicon.png": "e68768d2e5e2d1c0c9dcc6ad5fa203e3",
 "version.json": "dee7f318940621a14cd4efc2b872caae",
@@ -21,7 +21,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"main.dart.js": "56af3a43e2fc662987ce9da557377b39"
+"main.dart.js": "966577dd97ffcc41d1860aad9b5bd5eb"
 };
 
 // The application shell files that are downloaded before a service worker can
